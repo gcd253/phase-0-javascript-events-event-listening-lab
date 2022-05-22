92734c6bdfa93100ce767c715757665988bc6ec8
Griffin Dooley <dooleygriffin@gmail.com>
2022-05-22 16:28:26 -0400

    commit tree 5b43c29b8ed120204c31ac6655c5864884a4d3ce

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const makeAlert = document.getElementById('input')
+    makeAlert.addEventListener('click', (e) => alert("I was clicked!"))
 }
+
+addingEventListener()
